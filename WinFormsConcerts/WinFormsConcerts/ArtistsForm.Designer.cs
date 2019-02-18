6860a333f2746b1270e759f1b892f9889d09877c
Ксюша <@DESKTOP-KGJ7JQN>
2019-02-18 18:46:42 +0200

    Changes

--- WinFormsConcerts/WinFormsConcerts/ArtistsForm.Designer.cs
+++ WinFormsConcerts/WinFormsConcerts/ArtistsForm.Designer.cs
@@ -33,6 +33,10 @@
             this.dsConcerts = new ConcertsData.DSConcerts();
             this.bindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.dataGridViewArtistCategory = new System.Windows.Forms.DataGridView();
+            this.textBoxGenre = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelArtistName = new System.Windows.Forms.Label();
+            this.labelArtistGenre = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dsConcerts)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewArtistCategory)).BeginInit();
@@ -62,18 +66,58 @@
             this.dataGridViewArtistCategory.Size = new System.Drawing.Size(720, 269);
             this.dataGridViewArtistCategory.TabIndex = 0;
             // 
+            // textBoxGenre
+            // 
+            this.textBoxGenre.Location = new System.Drawing.Point(252, 89);
+            this.textBoxGenre.Name = "textBoxGenre";
+            this.textBoxGenre.ShortcutsEnabled = false;
+            this.textBoxGenre.Size = new System.Drawing.Size(180, 22);
+            this.textBoxGenre.TabIndex = 1;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(252, 38);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(180, 22);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // labelArtistName
+            // 
+            this.labelArtistName.AutoSize = true;
+            this.labelArtistName.Location = new System.Drawing.Point(70, 42);
+            this.labelArtistName.Name = "labelArtistName";
+            this.labelArtistName.Size = new System.Drawing.Size(48, 17);
+            this.labelArtistName.TabIndex = 3;
+            this.labelArtistName.Text = "Назва";
+            this.labelArtistName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelArtistGenre
+            // 
+            this.labelArtistGenre.AutoSize = true;
+            this.labelArtistGenre.Location = new System.Drawing.Point(73, 89);
+            this.labelArtistGenre.Name = "labelArtistGenre";
+            this.labelArtistGenre.Size = new System.Drawing.Size(45, 17);
+            this.labelArtistGenre.TabIndex = 4;
+            this.labelArtistGenre.Text = "Жанр";
+            // 
             // ArtistsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelArtistGenre);
+            this.Controls.Add(this.labelArtistName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxGenre);
             this.Controls.Add(this.dataGridViewArtistCategory);
             this.Name = "ArtistsForm";
             this.Text = "ArtistsForm";
+            this.Load += new System.EventHandler(this.ArtistsForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dsConcerts)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewArtistCategory)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,5 +127,9 @@
         private ConcertsData.DSConcerts dsConcerts;
         private System.Windows.Forms.BindingSource bindingSource;
         private System.Windows.Forms.DataGridView dataGridViewArtistCategory;
+        private System.Windows.Forms.TextBox textBoxGenre;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelArtistName;
+        private System.Windows.Forms.Label labelArtistGenre;
     }
 }
--- WinFormsConcerts/WinFormsConcerts/ArtistsForm.cs
+++ WinFormsConcerts/WinFormsConcerts/ArtistsForm.cs
@@ -12,9 +12,42 @@ namespace WinFormsConcerts
 {
     public partial class ArtistsForm : Form
     {
+        int id_cat;
+        string name_cat;
         public ArtistsForm()
         {
             InitializeComponent();
+        }
+
+        public ArtistsForm(int cat_id, string cat_name)
+        {
+            InitializeComponent();
+            Text = String.Concat(Text, " ", cat_name);
+
+            id_cat = cat_id;
+            name_cat = cat_name;
+            find_Artists();
+        }
+
+        private void find_Artists()
+        {
+            string findName = "%" + textBoxName.Text + "%";
+            if (textBoxGenre.Text != "")
+            {
+                string findGenre = "%" + textBoxGenre.Text + "%";
+
+                artistsByCategoryTableAdapter.FillByArtistGenre(dsConcerts.ArtistsByCategory, id_cat, findGenre);
+            }
+        }
+
+        private void ArtistsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
